c9b7ba559a77710cbe7b0e1c78341ccd340c1f98
cqw1 <cqw1@rice.edu>
2016-12-03 08:50:05 -0600

    can get basic palliassist website up. doesn't have boostrap css though

--- palliassist_webportal/app/views.py
+++ palliassist_webportal/app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index")
+    #return HttpResponse("Hello, world. You're at the index")
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+
+    context = {
+        'title': 'Home Page',
+        'year' : datetime.now().year,
+    }
+
+    return render(request, 'app/index.html', context)
 
