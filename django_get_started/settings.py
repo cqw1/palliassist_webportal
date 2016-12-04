eba7c7bbc3064e836c0ccd71a6f3f7a97ee2ef4e
cqw1 <cqw1@rice.edu>
2016-12-04 11:59:21 -0600

    changed staticfiles_dir away from local path

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -86,7 +86,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/cwang/palliassist_web/palliassist_webportal/app/static",
+    path.join(path.dirname(__file__), 'templates'),
 )
 
 # List of finder classes that know how to find static files in
