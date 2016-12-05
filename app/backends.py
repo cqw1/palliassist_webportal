c3f55884ce03d242d550a2047495fe47aaadc199
cqw1 <cqw1@rice.edu>
2016-12-05 00:02:32 -0600

    log in is connected to db now

--- app/backends.py
+++ app/backends.py
@@ -1,5 +1,6 @@
 # import the User object
 from django.contrib.auth.models import User
+import MySQLdb
 
 # verify against mysql db
 class SqlBackend:
@@ -7,11 +8,33 @@ class SqlBackend:
     # create an authentication method
     # this is called by the standard django login procedure
     def authenticate(self, username=None, password=None):
+        db = MySQLdb.connect(host="us-cdbr-azure-southcentral-f.cloudapp.net", user="b811fcf3c52d36", passwd="91e7ba1e", db="palliative")
+        cur = db.cursor()
+
+        found = cur.execute('SELECT * FROM palliative.login WHERE username = "' + username + '" AND password = "' + password + '"')
+
+        if found == 0:
+            # No user with the input username was found.
+            return None
+
+        try:
+            # User existed.
+            user = User.objects.get(username=username)
+            return user
+
+        except User.DoesNotExist:
+            # Creating new user.
+            user = User(username=username)
+            user.password = password
+            user.save()
+            return user
+        return None
+
 
         """
         try:
             # try to find a user matching the username
-            user = User.objects.get(username=username)
+            user = User.objects.get(username=username_input)
 
             # check the password is the reverse of the username
             if password == username[::-1]:
@@ -26,19 +49,8 @@ class SqlBackend:
         except User.DoesNotExist:
             # no user was found, return none - triggers default login failed
             return None
-        """
-        if password == username[::-1]:
-            try:
-                user = User.objects.get(username=username)
-                return user
 
-            except User.DoesNotExist:
-                user = User(username=username)
-                user.is_staff = True
-                user.is_superuser = True
-                user.save()
-                return user
-        return None
+        """
 
 
     # required for backend to work properly - unchanged in most scenarios
