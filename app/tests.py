6d8f936c550d064209f5cbdae6ef3618989a14bf
cqw1 <cqw1@rice.edu>
2016-12-06 00:15:29 -0600

    made db test

--- app/tests.py
+++ app/tests.py
@@ -5,6 +5,7 @@ when you run "manage.py test".
 
 import django
 from django.test import TestCase
+from redcap import Project, RedcapError
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
@@ -37,7 +38,18 @@ class ViewTest(TestCase):
         print "test_about: PASSED"
 
     def test_messaging(self):
-        """Tests the about page."""
+        """Tests the messaging page."""
         response = self.client.get('/messaging')
         self.assertContains(response, 'Messaging', 3, 200)
         print "test_messaging: PASSED"
+
+    def test_redcap_connection(self):
+        """Tests connecting to the REDCap database."""
+
+        URL = 'https://hcbredcap.com.br/api/'
+        TOKEN = 'F2C5AEE8A2594B0A9E442EE91C56CC7A'
+
+        project = Project(URL, TOKEN)
+        self.assertIsNotNone(project)
+
+        print "test_redcap_connenction: PASSED"
